4fdf50abfe77f4d7210628605c64e17fd35b2bef
Yoshterra1 <44651405+Yoshterra1@users.noreply.github.com>
2019-11-03 21:12:47 -0500

    Navbar component creation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route, BrowserRouter } from "react-router-dom";
 
 import Home from "./pages/home";
+import Navbar from './components/navbar.jsx'
 
 import logo from './logo.svg';
 import './App.css';
@@ -11,6 +12,7 @@ class App extends Component {
   render() { 
     return (  
       <React.Fragment>
+      <Navbar/>
       <main className="container">
         <BrowserRouter>
         <Route path="/" component={Home} />
