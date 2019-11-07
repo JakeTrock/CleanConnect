f37c7a5a941b617b222b9bd1a3ca0ea0e5783d4c
Yoshterra1 <44651405+Yoshterra1@users.noreply.github.com>
2019-11-07 09:18:36 -0500

    Increased functionality and expiration date detrmination in authentication

--- frontend/src/services/authentication.js
+++ frontend/src/services/authentication.js
@@ -4,15 +4,35 @@ import { apiUrl } from "../config.json"; // Url of the server
 
 const apiEndpoint = apiUrl + "/user";
 const tokenKey = "token";
+
 axios.defaults.headers.common["x-auth-token"] = getJwt();
 
 export async function login(email, password) {
   const { data: jwt } = await axios.post(apiEndpoint+"/login", { email, password });
-  console.log(jwt)
-  localStorage.setItem(tokenKey, jwt);
+  localStorage.setItem(tokenKey, jwt.token); //get expiration date 
 }
 
 export function getJwt() { //Session is stored in localhost
+  const jwt=localStorage.getItem(tokenKey);
+  if(jwtDecode(jwt).exp*1000<Date.now()){
+    console.log("This will need to be dealt with soon")
+    //localStorage.removeItem(tokenKey)
+    return null
+  }
   return localStorage.getItem(tokenKey);
 }
 
+export function getCurrentUser() {
+  try {
+    const jwt = localStorage.getItem(tokenKey);
+    return jwtDecode(jwt);
+  } catch (ex) {
+    return null;
+  }
+}
+
+export default {
+  login,
+  getCurrentUser,
+  getJwt
+};
