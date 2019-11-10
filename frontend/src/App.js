cffe75ad625b7bd68b5a332a3a6b9bac0901f2be
Yoshterra1 <44651405+Yoshterra1@users.noreply.github.com>
2019-11-10 11:14:44 -0500

    Now passes user information

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,21 +3,32 @@ import { Route, Switch } from "react-router-dom";
 
 import Home from "./pages/home";
 import Login from "./pages/login";
+import Logout from "./pages/logout";
+import Register from "./pages/register";
 
-import Navbar from './components/navbar.jsx'
 
+import Navbar from './components/navbar'
+
+import auth from './services/authentication'
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   state = {  }
+  componentDidMount() {
+    const user = auth.getCurrentUser();
+    this.setState({ user });
+  }
   render() { 
+    const { user } = this.state;
     return (  
       <React.Fragment>
-      <Navbar/> {/* Displays navbar on all pages (may change later) */}
+      <Navbar user = {user}/> {/* Displays navbar on all pages (may change later) */}
       <main className="container">
         <Switch> {/* Allows a single page website to connect to all pages */}
         <Route path="/login" component={Login}/>
+        <Route path="/register" component={Register}/>
+        <Route path="/logout" component={Logout}/>
         <Route path="/" component={Home} />
         </Switch>
       </main>
