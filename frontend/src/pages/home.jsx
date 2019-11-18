754530c72aa4822a14e746a5d2940af0efad8fce
Yoshterra1 <44651405+Yoshterra1@users.noreply.github.com>
2019-11-18 08:51:08 -0500

    Created temporary Carousel for front page

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+
+import Carousel from  '../components/carousel';
+
 class Home extends Component {
   state = {  }
   render() { 
     return (  
       <React.Fragment>
+        <Carousel/>
+        <p> Welcome to Clean Connect!</p>
       </React.Fragment>
     );
   }
