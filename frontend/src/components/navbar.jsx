108dcfb67bd4cb1cf6c13a9ffe936446fb1b0a4c
Yoshterra1 <44651405+Yoshterra1@users.noreply.github.com>
2019-11-18 08:49:45 -0500

    Added logo to navbar and moved links to right side of navbar

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Link, NavLink } from "react-router-dom";
+
+import logo from '../images/logo.png';
 //Link and NavLink are react specific tools allowing you to move through pages of a single paged website
 const Navbar = ({ user }) => {
   //Temporary navbar, subject to change
   return ( 
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <Link className="navbar-brand" to="/">
-      Clean Connect
-    </Link>
+    <img src = {logo}/>
     <button
       className="navbar-toggler"
       type="button"
@@ -19,14 +19,14 @@ const Navbar = ({ user }) => {
     >
       <span className="navbar-toggler-icon" />
     </button>
-    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div className="navbar-nav ml-auto" id="navbarNavAltMarkup">
       <div className="navbar-nav">
-      {!user && (
+          {!user && (
             <React.Fragment>
-              <NavLink className="nav-item nav-link" to="/login">
+              <NavLink className="nav-item nav-link navbar" to="/login">
                 Login
               </NavLink>
-              <NavLink className="nav-item nav-link" to="/register">
+             <NavLink className="nav-item nav-link"  to="/register">
                 Register
               </NavLink>
             </React.Fragment>
@@ -41,6 +41,7 @@ const Navbar = ({ user }) => {
               </NavLink>
             </React.Fragment>
           )}
+         
       </div>
     </div>
   </nav>
