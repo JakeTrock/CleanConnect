5313401cf2e7d008e46973836fd0203a50f063f3
Yoshterra1 <44651405+Yoshterra1@users.noreply.github.com>
2019-11-21 11:06:56 -0500

    Adding new routes based on added functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,25 @@
 import React, { Component } from 'react';
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 
+import Dashboard from "./pages/dashboard"
 import Home from "./pages/home";
 import Login from "./pages/login";
 import Logout from "./pages/logout";
 import Register from "./pages/register";
-
+import Profile from "./pages/profile";
+import NotFound from "./pages/notFound";
 
 import Navbar from './components/navbar'
-
+import ProtectedRoute from './components/protectedRoute'
+import NoTokenRoute from './components/noTokenRoute'
 import auth from './services/authentication'
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   state = {  }
   componentDidMount() {
     const user = auth.getCurrentUser();
-    this.setState({ user });
+    this.setState({user});
   }
   render() { 
     const { user } = this.state;
@@ -26,10 +28,19 @@ class App extends Component {
       <Navbar user = {user}/> {/* Displays navbar on all pages (may change later) */}
       <main className="container">
         <Switch> {/* Allows a single page website to connect to all pages */}
-        <Route path="/login" component={Login}/>
-        <Route path="/register" component={Register}/>
-        <Route path="/logout" component={Logout}/>
-        <Route path="/" component={Home} />
+        <NoTokenRoute path="/login" component={Login}/>
+        <NoTokenRoute path="/register" component={Register}/>
+        <ProtectedRoute path="/logout" component={Logout}/>
+        <ProtectedRoute path="/change/:token" component={Login}/> {/* Will not be login in the future */}
+        <ProtectedRoute path="/profile" 
+        render={props=> <Profile {...props} user={user}/>}/>
+        {user && 
+        <ProtectedRoute exact path="/" 
+          render={props=> <Dashboard {...props} user={user}/>}/>
+        }
+        {!user && <Route exact path="/" component={Home} />}
+        <Route path="/notFound" component= {NotFound}/>
+        <Redirect to ="/notFound"/>
         </Switch>
       </main>
       </React.Fragment>
