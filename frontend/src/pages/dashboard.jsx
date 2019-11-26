4374e4761a7d4e69a1265a6bdd3ad52f3474a60c
Yoshterra1 <44651405+Yoshterra1@users.noreply.github.com>
2019-11-26 07:33:09 -0500

    Updating dashboard to also include layout and a general idea for how to render issues

--- frontend/src/pages/dashboard.jsx
+++ frontend/src/pages/dashboard.jsx
@@ -1,15 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Layout from "../components/layout";
 
 class Dashboard extends Component {
-  state = { 
-    problems:[],
-    user:this.props.user
-   }
-  render() { 
-    return ( 
-    <div></div> 
+  state = {
+    issues: [],
+    user: this.props.user
+  };
+  componentDidMount() {
+    //call route in backend to get issues depending on user
+  }
+  render() {
+    const { issues } = this.state;
+    return (
+      <Layout name="Issue Tracker">
+        {issues.map(issue => (
+          <div>{issue}</div>
+        ))}
+      </Layout>
     );
   }
 }
- 
+
 export default Dashboard;
