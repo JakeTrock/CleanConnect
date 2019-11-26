396a7596f793dbb31d8c55c87b6967de066d3c63
Yoshterra1 <44651405+Yoshterra1@users.noreply.github.com>
2019-11-26 07:36:15 -0500

    Implementing working barebones Carousel

--- frontend/src/components/carousel.jsx
+++ frontend/src/components/carousel.jsx
@@ -1,20 +1,49 @@
-import React, { Component } from 'react';
-import logo from '../images/logo.png';
-import '../css/carousel.css'
+import React, { Component } from "react";
+import logo from "../images/logo2.png";
+import logo2 from "../images/logo.png";
+
+import "../css/carousel.css";
 
 class Carousel extends Component {
-  state = {  }
-  render() { 
-    return ( 
-    <div className = "carouselContainer">
-        <span className="left">&#60;</span>
-        <span className="center">
-        <img src={logo} alt=""/>
+  state = {
+    images: [logo, logo2, logo],
+    positionInList: 0
+  };
+  changeImage(direction) {
+    if (this.listBoundary(direction)) {
+      const positionInList = this.state.positionInList + direction;
+      this.setState({ positionInList });
+    }
+  }
+
+  listBoundary(direction) {
+    if (
+      direction + this.state.positionInList > this.state.images.length - 1 ||
+      direction + this.state.positionInList < 0
+    )
+      return false;
+    return true;
+  }
+  render() {
+    const { images, positionInList } = this.state;
+    return (
+      <div className="carouselContainer mt-5">
+        <span
+          role="button"
+          className="direction"
+          onClick={() => this.changeImage(-1)}
+        >
+          {this.listBoundary(-1) && <div>&#60;</div>}
         </span>
-    </div>
-     );
+        <span className="center">
+          <img className="centerImg" alt="" src={images[positionInList]}></img>
+        </span>
+        <span className="direction" onClick={() => this.changeImage(1)}>
+          {this.listBoundary(1) && <div>&#62;</div>}
+        </span>
+      </div>
+    );
   }
 }
- 
-export default Carousel;
 
+export default Carousel;
