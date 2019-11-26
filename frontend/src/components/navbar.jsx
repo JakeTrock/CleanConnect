a0286ef5cee5892e0918af4548cacca4a8bdd86d
Yoshterra1 <44651405+Yoshterra1@users.noreply.github.com>
2019-11-26 07:37:18 -0500

    Initial tags file creation which is accessed by Navbar

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,33 +1,35 @@
-import React from 'react';
-import { NavLink } from "react-router-dom";
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
 
-import logo from '../images/logo.png';
+import logo from "../images/logo.png";
 //Link and NavLink are react specific tools allowing you to move through pages of a single paged website
 const Navbar = ({ user }) => {
   //Temporary navbar, subject to change
-  return ( 
+  return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <img src = {logo} style = {{width:"30%"}} alt=""/>
-    <button
-      className="navbar-toggler"
-      type="button"
-      data-toggle="collapse"
-      data-target="#navbarNavAltMarkup"
-      aria-controls="navbarNavAltMarkup"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span className="navbar-toggler-icon" />
-    </button>
-    
-    <div className="navbar-collapse collapse" id="navbarNavAltMarkup">
-      <div className="nav navbar-nav ml-auto">
+      <Link style={{ width: "50%" }} to="/">
+        <img style={{ width: "60%" }} src={logo} alt="" />
+      </Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon" />
+      </button>
+
+      <div className="navbar-collapse collapse" id="navbarNavAltMarkup">
+        <div className="nav navbar-nav ml-auto">
           {!user && (
             <React.Fragment>
               <NavLink className="nav-item nav-link navbar" to="/login">
                 Login
               </NavLink>
-              <NavLink className="nav-item nav-link"  to="/register">
+              <NavLink className="nav-item nav-link" to="/register">
                 Register
               </NavLink>
             </React.Fragment>
@@ -37,17 +39,18 @@ const Navbar = ({ user }) => {
               <NavLink className="nav-item nav-link" to="/profile">
                 {user.name}
               </NavLink>
+              <NavLink className="nav-item nav-link" to="/tags">
+                Tags
+              </NavLink>
               <NavLink className="nav-item nav-link" to="/logout">
                 Logout
               </NavLink>
-              
             </React.Fragment>
           )}
-         
+        </div>
       </div>
-    </div>
-  </nav>
+    </nav>
   );
-}
- 
+};
+
 export default Navbar;
---
+++ frontend/src/pages/tags.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+class Tags extends Component {
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <p>E</p>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Tags;
