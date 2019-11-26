951810047eb07284e5d6a91fba3f18cb74b09498
Yoshterra1 <44651405+Yoshterra1@users.noreply.github.com>
2019-11-26 07:23:04 -0500

    Installing React Toastify to display Error/Success Messages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-import Dashboard from "./pages/dashboard"
+import Dashboard from "./pages/dashboard";
 import Home from "./pages/home";
 import Login from "./pages/login";
 import Logout from "./pages/logout";
@@ -9,44 +11,49 @@ import Register from "./pages/register";
 import Profile from "./pages/profile";
 import NotFound from "./pages/notFound";
 
-import Navbar from './components/navbar'
-import ProtectedRoute from './components/protectedRoute'
-import NoTokenRoute from './components/noTokenRoute'
-import auth from './services/authentication'
-import './App.css';
+import Navbar from "./components/navbar";
+import ProtectedRoute from "./components/protectedRoute";
+import NoTokenRoute from "./components/noTokenRoute";
+import auth from "./services/authentication";
+import "./App.css";
 
 class App extends Component {
-  state = {  }
+  state = {};
   componentDidMount() {
     const user = auth.getCurrentUser();
-    this.setState({user});
+    this.setState({ user });
   }
-  render() { 
+  render() {
     const { user } = this.state;
-    return (  
+    return (
       <React.Fragment>
-      <Navbar user = {user}/> {/* Displays navbar on all pages (may change later) */}
-      <main className="container">
-        <Switch> {/* Allows a single page website to connect to all pages */}
-        <NoTokenRoute path="/login" component={Login}/>
-        <NoTokenRoute path="/register" component={Register}/>
-        <ProtectedRoute path="/logout" component={Logout}/>
-        <ProtectedRoute path="/change/:token" component={Login}/> {/* Will not be login in the future */}
-        <ProtectedRoute path="/profile" 
-        render={props=> <Profile {...props} user={user}/>}/>
-        {user && 
-        <ProtectedRoute exact path="/" 
-          render={props=> <Dashboard {...props} user={user}/>}/>
-        }
-        {!user && <Route exact path="/" component={Home} />}
-        <Route path="/notFound" component= {NotFound}/>
-        <Redirect to ="/notFound"/>
-        </Switch>
-      </main>
+        <ToastContainer />
+        <Navbar user={user} />
+        {/* Displays navbar on all pages (may change later) */}
+        <main className="container">
+          <Switch>
+            {/* Allows a single page website to connect to all pages */}
+            <NoTokenRoute path="/login" component={Login} />
+            <NoTokenRoute path="/register" component={Register} />
+            <ProtectedRoute path="/logout" component={Logout} />
+            <ProtectedRoute
+              path="/profile"
+              render={props => <Profile {...props} user={user} />}
+            />
+            {user && (
+              <ProtectedRoute
+                exact
+                path="/"
+                render={props => <Dashboard {...props} user={user} />}
+              />
+            )}
+            <Route path="/notFound" component={NotFound} />
+            <Redirect to="/notFound" />
+          </Switch>
+        </main>
       </React.Fragment>
     );
   }
 }
- 
-export default App;
 
+export default App;
--- frontend/src/services/authentication.js
+++ frontend/src/services/authentication.js
@@ -1,36 +1,58 @@
-import jwtDecode from "jwt-decode";
+import jwtDecode from "jwt-decode";
 import axios from "axios"; //Will be used for http requests
-import { apiUrl } from "../config.json"; // Url of the server
+import { toast } from "react-toastify";
 
-const apiEndpoint = apiUrl + "/user";
-const tokenKey = "token";
+import { apiUrl } from "../config.json"; // Url of the server
 
+const apiEndpoint = apiUrl + "/user";
+const tokenKey = "token";
 
-export async function login(email, password) {
-  const { data: jwt } = await axios.post(apiEndpoint+"/login", { email, password });
-  localStorage.setItem(tokenKey, jwt.token); //get expiration date 
+axios.interceptors.response.use(
+  function(response) {
+    console.clear();
+    if (response.data.status && response.data)
+      toast.success(response.data.status);
+    return response;
+  },
+  function(error) {
+    console.clear();
+    toast.error(error.response.statusText, { autoClose: 2500 });
+    return Promise.reject(error);
+  }
+);
+export async function login(email, password) {
+  const { data: jwt } = await axios.post(apiEndpoint + "/login", {
+    email,
+    password
+  });
+  localStorage.setItem(tokenKey, jwt.token); //get expiration date
 }
 
-export async function register(name, email, password, password2) {
-  await axios.post(apiEndpoint+"/register", { name, email, password, password2 });
-   //registers then logins current user 
-   //will change when email verification is completed
+export async function register(name, email, password, password2) {
+  await axios.post(apiEndpoint + "/register", {
+    name,
+    email,
+    password,
+    password2
+  });
 }
 
-export function logout(){
+export function logout() {
   localStorage.removeItem(tokenKey);
 }
 
-export function getCurrentUser() { //Session is stored in localhost
+export function getCurrentUser(noDecode) {
+  //Session is stored in localhost
   try {
     const jwt = localStorage.getItem(tokenKey); //searches local storage for jwt key
-    if(jwtDecode(jwt).exp*1000<Date.now()){ //if passed expiration, delete
-      logout()
-      return null
+    if (jwtDecode(jwt).exp * 1000 < Date.now()) {
+      //if passed expiration, delete
+      logout();
+      return null;
     }
-    return jwtDecode(jwt);
-  } 
-  catch (ex) {
+    if (noDecode) return jwt;
+    else return jwtDecode(jwt);
+  } catch (ex) {
     return null;
   }
 }
@@ -38,5 +60,5 @@ export function getCurrentUser() { //Session is stored in localhost
 export default {
   login,
   getCurrentUser,
-  logout,
+  logout
 };
