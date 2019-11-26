06a651cb57487aed9aa1196f1edb5d32705c785c
Yoshterra1 <44651405+Yoshterra1@users.noreply.github.com>
2019-11-26 07:29:18 -0500

    Calling layout style and general cleanup

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React from "react";
+
 import Joi from "joi-browser";
+
 import Form from "../components/form"; //allows you to render Input, initalizing login form as a form
-import * as auth from "../services/authentication";
+import Layout from "../components/layout";
+import * as auth from "../services/authentication";
 
 class Login extends Form {
-  state = {  //data stored in the form
+  state = {
+    //data stored in the form
     data: { email: "", password: "" },
     errors: {}
-  }
-  schema = { //using Joi for form creation and errors (change?)
+  };
+  schema = {
+    //using Joi for form creation and errors (change?)
     email: Joi.string()
       .required()
       .label("Email"),
@@ -16,41 +21,43 @@ class Login extends Form {
       .required()
       .label("Password")
   };
-  doSubmit = async () => {
-    try {
-      const { data } = this.state;
-      await auth.login(data.email, data.password);
-      const { state } = this.props.location;
-      window.location = state ? state.from.pathname : "/";
-    } 
-    catch (ex) {
-      console.clear()
-      if (ex.response){
-        const errors = { ...this.state.errors };
-        if(ex.response.status === 400) {
-          errors.email = ex.response.data.email;
-          errors.password = ex.response.data.password;
-        }
-        if(ex.response.status === 401){
-          errors.email=ex.response.data.msg;
+  doSubmit = async () => {
+    try {
+      const { data } = this.state;
+      await auth.login(data.email, data.password);
+      const { state } = this.props.location;
+      window.location = state ? state.from.pathname : "/";
+    } catch (ex) {
+      if (ex.response) {
+        const errors = { ...this.state.errors };
+        if (ex.response.status === 400) {
+          errors.email = ex.response.data.email;
+          errors.password = ex.response.data.password;
         }
-        this.setState({ errors });
+        if (ex.response.status === 401) {
+          errors.email = ex.response.data.msg;
+        }
+        this.setState({ errors });
       }
-    }
-  };
-  render() { 
-    const errors=this.state.errors
-    return ( 
-      <div>
-        <h1>Login</h1>
+    }
+  };
+  render() {
+    const errors = this.state.errors;
+    return (
+      <Layout name="Login">
         <form onSubmit={this.handleSubmit}>
-          {this.renderInput("email", "Email",errors.email)}
-          {this.renderInput("password", "Password",errors.password,"password")}
+          {this.renderInput("email", "Email", errors.email)}
+          {this.renderInput(
+            "password",
+            "Password",
+            errors.password,
+            "password"
+          )}
           {this.renderButton("Login")}
         </form>
-      </div>
-     );
+      </Layout>
+    );
   }
 }
- 
+
 export default Login;
--- frontend/src/pages/register.jsx
+++ frontend/src/pages/register.jsx
@@ -1,58 +1,69 @@
-import React from 'react';
+import React from "react";
+import { Redirect } from "react-router-dom";
+
 import Joi from "joi-browser";
+
 import Form from "../components/form"; //allows you to render Input, initalizing login form as a form
-import * as auth from "../services/authentication";
+import Layout from "../components/layout";
+import * as auth from "../services/authentication";
 
 class Register extends Form {
-  state = {  //data stored in the form
-    data: { name:"",email: "", password: "", password2:""},
-    errors: {}
-  }
-  schema = { //using Joi for form creation and errors (change?)
-    name: Joi.string()
-      .required(),
-    email: Joi.string().email()
-      .required(),
-    password: Joi.string()
-      .required(),
-    password2: Joi.string()
-      .required()
-
+  state = {
+    //data stored in the form
+    data: { name: "", email: "", password: "", password2: "" },
+    errors: {},
+    formCompleted: false
   };
-  doSubmit = async () => {
-    try {
-      const { data } = this.state;
-      await auth.register(data.name, data.email, data.password, data.password2);
-      const { state } = this.props.location;
-      window.location = state ? state.from.pathname : "/";
-    } 
-    catch (ex) {
-      console.clear()
-      if (ex.response && ex.response.status === 400) {
-        const errors = { ...this.state.errors };
-        errors.name = ex.response.data.name;
-        errors.email = ex.response.data.email;
-        errors.password = ex.response.data.password;
-        errors.password2 = ex.response.data.password2;
-        this.setState({ errors });
-      }
-    }
-  };
-  render() { 
-    const errors=this.state.errors
-    return ( 
-      <div>
-        <h1>Register</h1>
+  schema = {
+    //using Joi for form creation and errors (change?)
+    name: Joi.string().required(),
+    email: Joi.string()
+      .email()
+      .required(),
+    password: Joi.string().required(),
+    password2: Joi.string().required()
+  };
+  doSubmit = async () => {
+    try {
+      const { data } = this.state;
+      await auth.register(data.name, data.email, data.password, data.password2);
+      this.setState({ formCompleted: true }); //const { state } = this.props.location; //window.location = state ? state.from.pathname : "/login";
+    } catch (ex) {
+      if (ex.response && ex.response.status === 400) {
+        const errors = { ...this.state.errors };
+        errors.name = ex.response.data.name;
+        errors.email = ex.response.data.email;
+        errors.password = ex.response.data.password;
+        errors.password2 = ex.response.data.password2;
+        this.setState({ errors });
+      }
+    }
+  };
+  render() {
+    if (this.state.formCompleted === true) return <Redirect to="/login" />;
+    const errors = this.state.errors;
+    return (
+      <Layout name="Register">
         <form onSubmit={this.handleSubmit}>
-          {this.renderInput("name", "Name",errors.name)}
+          {this.renderInput("name", "Name", errors.name)}
           {this.renderInput("email", "Email", errors.email)}
-          {this.renderInput("password", "Password", errors.password,"password")}
-          {this.renderInput("password2", "Confirm Password",errors.password2,"password")}
+          {this.renderInput(
+            "password",
+            "Password",
+            errors.password,
+            "password"
+          )}
+          {this.renderInput(
+            "password2",
+            "Confirm Password",
+            errors.password2,
+            "password"
+          )}
           {this.renderButton("Register")}
         </form>
-      </div>
-     );
+      </Layout>
+    );
   }
 }
- 
+
 export default Register;
