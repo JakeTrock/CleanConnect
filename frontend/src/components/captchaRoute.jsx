eaa9b636d6b0d2e8429b38c846f6b0c51e613beb
Yoshterra1 <44651405+Yoshterra1@users.noreply.github.com>
2020-01-10 10:28:46 -0500

    Creating captchaRoute, which requires reCaptcha authentication

---
+++ frontend/src/components/captchaRoute.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { Route } from "react-router-dom";
+
+import ReCAPTCHA from "react-google-recaptcha";
+
+const CaptchaRoute = ({
+  path,
+  validated,
+  captchaSubmit,
+  component: Component,
+  render,
+  ...rest
+}) => {
+  const recaptchaRef = React.createRef();
+
+  const captchaCompleted = value => {
+    if (value) captchaSubmit();
+  };
+  const Captcha = () => {
+    return (
+      <div>
+        <h3 align="center" style={{ fontSize: "6vw" }} className="mt-5 mb-5">
+          Finish the captcha to access the page
+        </h3>
+        <ReCAPTCHA
+          size="compact"
+          ref={recaptchaRef}
+          sitekey="6LeQ3csUAAAAAJMNvvh313yU9HCborrUzc4bzQ5t"
+          onChange={captchaCompleted}
+          align="center"
+        />
+      </div>
+    );
+  };
+  return (
+    <React.Fragment>
+      <Route
+        {...rest}
+        render={props => {
+          if (!validated)
+            return (
+              <React.Fragment>
+                <Captcha />
+              </React.Fragment>
+            );
+          return Component ? <Component {...props} /> : render(props);
+        }}
+      />
+    </React.Fragment>
+  );
+};
+
+export default CaptchaRoute;
