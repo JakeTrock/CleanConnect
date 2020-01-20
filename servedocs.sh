69edd7b906a8e796e281ecda090951a176511ed7
Jake <jtbosshku@gmail.com>
2020-01-19 20:19:06 -0500

    changed webhook methods

--- servedocs.sh
+++
@@ -1,8 +0,0 @@
-git checkout master;
-git fetch origin
-git reset --hard origin/master
-git pull;
-git status;
-cd backend && npm i && cd ..
-cd frontend && npm i && cd ..
-pm2 start ecosystem.config.js --watch
