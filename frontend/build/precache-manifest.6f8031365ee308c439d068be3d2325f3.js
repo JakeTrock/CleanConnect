28bc3372686fa879a4232f83fd68964f6c23c001
Jake <jtbosshku@gmail.com>
2020-01-22 16:30:30 -0500

    add new client build

---
+++ frontend/build/precache-manifest.6f8031365ee308c439d068be3d2325f3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a9f6c77a261943d82b0a46a031b8d00",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4724d74f7b46d565b271",
+    "url": "/static/css/2.54647d8a.chunk.css"
+  },
+  {
+    "revision": "df4e39f41d1ee43b4215",
+    "url": "/static/css/main.1108a25f.chunk.css"
+  },
+  {
+    "revision": "4724d74f7b46d565b271",
+    "url": "/static/js/2.a8977a5f.chunk.js"
+  },
+  {
+    "revision": "08fefedaab24d8dd1371108325f1f2ab",
+    "url": "/static/js/2.a8977a5f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "df4e39f41d1ee43b4215",
+    "url": "/static/js/main.9994a39a.chunk.js"
+  },
+  {
+    "revision": "05e2d2637e83c42dc136",
+    "url": "/static/js/runtime-main.4ee28378.js"
+  },
+  {
+    "revision": "4174065a2c851d5a0a19d2856304dea9",
+    "url": "/static/media/FuturaMedium.4174065a.ttf"
+  }
+]);
