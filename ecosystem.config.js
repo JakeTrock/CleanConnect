92233af59e8a635b7a877d1f071bfbf3d6107bee
Jake <jtbosshku@gmail.com>
2020-01-22 08:32:25 -0500

    changed server files

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps : [{
     name: 'API',
-    script: 'npm run dev',
+    script: 'npm run prod',
 
     // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
     args: 'one two',
--- productionUpdateServer.sh
+++ productionUpdateServer.sh
@@ -1,4 +1,8 @@
-cd backend
+cd frontend
 npm run build
+git add build
+git commit -m "add new client build"
+git push https://hokuco:Miweb1Hi@github.com/hokuco/CleanConnect.git
 cd ..
+
 ssh -i "UKP.pem" ubuntu@ec2-3-20-85-15.us-east-2.compute.amazonaws.com 'cd && cd noderoot/CleanConnect/ && git pull https://hokuco:Miweb1Hi@github.com/hokuco/CleanConnect.git && cd backend && npm i && cd .. && cd frontend && npm i && cd .. && pm2 restart ecosystem.config.js --watch && exit'
