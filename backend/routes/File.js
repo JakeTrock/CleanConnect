d67c5b3adeebe885dfaa776a97acbe5aa58c592f
Jake <jtbosshku@gmail.com>
2020-02-07 12:34:43 -0500

    change location of cron systems

--- backend/routes/File.js
+++ backend/routes/File.js
@@ -1,12 +1,10 @@
 //load in libraries
 const express = require('express');
-const fs = require('fs');
-const CronJob = require("cron").CronJob;
+
 //load in express modules
 const router = express.Router();
 //load in mongoose templates
-const User = require("../models/User");
-const UserIndex = require("../models/UserIndex.js");
+
 const tempDir = process.env.rootDir + '/temp/';
 // ROUTE: GET /pdf/:url
 // DESCRIPTION: retrieves and returns pdf from temp dir
@@ -32,42 +30,5 @@ router.get('/img/:url', (req, res) => {
         simple: "invalid url. url may be incorrectly typed, or file may no longer exist."
     });
 });
-
-// ROUTE: NONE
-// DESCRIPTION: deletes unused tokens and pdfs over a week old from the server
-// INPUT: NONE
-const delExp = new CronJob("00 00 00 * * *", function() {
-    console.log("Goodnight, time to delete some stuff! (-_-)ᶻᶻᶻᶻ");
-    var d = new Date();
-    d.setDate(d.getDate() - 7);
-    UserIndex.find({
-        isCritical: true,
-        created_at: { $lt: d }
-    }, function(err, list) {
-        for (var i = 0; i < list.length; i++) {
-            if (err) console.log(err);
-            console.log(list[i]);
-            User.findOneAndRemove({
-                isVerified: false,
-                _id: list[i]._userId
-            });
-            UserIndex.findOneAndRemove({
-                isCritical: true,
-                created_at: { $lt: d },
-                _id: list[i]._userId
-            });
-        }
-    });
-    fs.readdir(tempDir, function(err, files) {
-        files.forEach(function(file) {
-            if (fs.statSync(tempDir + file).birthtime < d && file.split(".")[1] != "pathpreserve") {
-                fs.unlinkSync(tempDir + file);
-            }
-        });
-    });
-}, null, true, 'America/New_York');
-
-//start cronjob
-delExp.start();
 //exports current script as module
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,19 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const cors = require('cors');
+const fs = require('fs');
+const CronJob = require("cron").CronJob;
+const User = require("./models/User");
+const UserIndex = require("./models/UserIndex.js");
+const Tag = require("./models/Tag.js");
+const Comment = require('./models/Comment.js');
+
 //import keys and creds, exp envvars
 const keys = require('./config/keys');
 process.env.rootDir = __dirname;
+process.env.topLevelDomain = "cleanconnect.jakesandbox.com";
+process.env.domainPrefix = "https://";
+
 process.env.mailCreds = [process.env.mailServer || keys.mailServer, process.env.mailPort || keys.mailPort, process.env.mailUser || keys.mailUser, process.env.mailPass || keys.mailPass];
 //imports different router/handler files
 const user = require('./routes/User.js');
@@ -43,19 +53,91 @@ app.use('/file', file);
 const port = process.env.BEPORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
 
+// ROUTE: NONE
+// DESCRIPTION: deletes unused tokens and pdfs over a week old from the server
+// INPUT: NONE
+const tempDir = __dirname + '/temp/';
+const delExp = new CronJob("00 00 00 * * *", function() {
+    console.log("Goodnight, time to delete some stuff! (-_-)ᶻᶻᶻᶻ");
+    //older than one week block
+    var d = new Date();
+    d.setDate(d.getDate() - 7);
+    UserIndex.find({
+        isCritical: true,
+        createdAt: { $lt: d }
+    }, function(err, list) {
+        for (var i = 0; i < list.length; i++) {
+            if (err) console.log(err);
+            console.log(list[i]);
+            User.findOneAndRemove({
+                isVerified: false,
+                _id: list[i]._userId
+            });
+            UserIndex.findOneAndRemove({
+                isCritical: true,
+                createdAt: { $lt: d },
+                _id: list[i]._userId
+            });
+        }
+    });
+    fs.readdir(tempDir, function(err, files) {
+        files.forEach(function(file) {
+            if (fs.statSync(tempDir + file).birthtime < d && file.split(".")[1] == "pdf") {
+                fs.unlinkSync(tempDir + file);
+            }
+        });
+    });
+    //older than one month block
+    d.setDate(d.getDate() - 23);
+    //remove images connected to stale comments
+    Comment.find({
+        markedForDeletion: true,
+        removedAt: { $lt: d }
+    }).then(cmts => {
+        if (cmts) {
+            for (var n in cmts) {
+                if (n.img) {
+                    fs.unlinkSync(tempDir + n.img);
+                }
+            }
+        }
+    });
+    //remove comments
+    Comment.deleteMany({
+        markedForDeletion: true,
+        removedAt: { $lt: d }
+    }).then(result => console.log(result)).catch(err => console.error(`Delete failed with error: ${err}`));
+    //older than 2 months block
+    d.setDate(d.getDate() - 30);
+    Tag.find({
+        markedForDeletion: true,
+        removedAt: { $lt: d }
+    }).then(tgs => {
+        if (tgs) {
+            for (var n in tgs) {
+                Comment.find({
+                    tag: n._id
+                }).then(cmts => {
+                    if (cmts) {
+                        for (var z in cmts) {
+                            if (z.img) {
+                                fs.unlinkSync(tempDir + z.img);
+                            }
+                        }
+                    }
+                });
+            }
+            Comment.deleteMany({
+                markedForDeletion: true,
+                removedAt: { $lt: d }
+            }).then(result => console.log(result)).catch(err => console.error(`Delete failed with error: ${err}`));
+        }
+    });
+    Tag.deleteMany({
+        markedForDeletion: true,
+        removedAt: { $lt: d }
+    }).then(result => console.log(result)).catch(err => console.error(`Delete failed with error: ${err}`));
+}, null, true, 'America/New_York');
 
-
-// var memwatch = require('node-memwatch');
-// var hd = new memwatch.HeapDiff();
-// memwatch.on('stats', function(stats) {
-//     console.log(JSON.stringify(stats));
-//     var diff = hd.end();
-//     console.log(JSON.stringify(diff));
-//     hd = new memwatch.HeapDiff();
-// });
-// memwatch.on('leak', function(info) {
-//     console.log(JSON.stringify(info));
-//     var diff = hd.end();
-//     console.log(JSON.stringify(diff));
-//     hd = new memwatch.HeapDiff();
-// });
+//start cronjob
+delExp.start();
