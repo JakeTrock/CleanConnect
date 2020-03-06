3e21c8cc80c0ac781d4703c9aa0e3452efeb7a3e
Jake <jtbosshku@gmail.com>
2020-03-06 09:08:35 -0500

    use new blacklist

--- backend/validation/apr.js
+++ backend/validation/apr.js
@@ -1,16 +1,17 @@
-const swear = require('./swearWord').list;
+// const swear = require('./swearWord').list;
 const Validator = require('validator');
 const isEmpty = require('./is-empty');
-
+var cuss = require('cuss');
 
 module.exports = function validatePostInput(data) {
     let errors = {};
+    let instr = data.text.toLowerCase().split(" ");
     if (Validator.isEmpty(data.text)) {
         errors.text = 'problem is required';
     }
-    if (swear.indexOf(data.text.toLowerCase()) > -1) {
-        errors.text = "You'll have to clean up your language before we clean up this room.";
-    }
+    for (var b in instr)
+        if (cuss[instr[b]] == 2)
+            errors.text = "You'll have to clean up your language before we clean up this room.";
     return {
         errors,
         isValid: isEmpty(errors)
