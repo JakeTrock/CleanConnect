25250a1f4b47659fd8afef5124eca756781ed9cd
Yoshterra1 <44651405+Yoshterra1@users.noreply.github.com>
2020-03-10 13:59:41 -0400

    Updated tag.jsx to use updated CallbackPopupContainer

--- frontend/src/pages/tags.jsx
+++ frontend/src/pages/tags.jsx
@@ -5,7 +5,7 @@ import Layout from "../components/layout";
 import Grid from "../components/grid";
 import Unit from "../components/unit";
 import {
-  DeletePopupContainer,
+  CallbackPopupContainer,
   ImageContainer
 } from "../components/popupContainer";
 import Pagination from "../components/pagination";
@@ -16,72 +16,60 @@ import "../css/unit.css";
 const queryString = require("query-string");
 class Tags extends Component {
   state = {
-    tags: [],
-    limit: "",
+    tags: "",
+    limit: this.setLimit(),
     currentPage: 1,
     pageSize: 6
   };
-  async stateSetter() {
-    let { user } = this.props;
-    let tags = "";
-    if (user) {
-      tags = await auth.getTags();
-      for (let i = 0; i < tags.length; i++) {
-        //contingency if a tag doesn't have a qr code
-        if (!tags[i].qrcode) {
-          await auth.genCache();
-        }
-      }
-      tags = tags.data.reverse();
-      let limit = null;
-      if (user.tier === 0) limit = 5;
-      if (user.tier === 1) limit = 25;
-      if (limit !== this.state.limit) this.setState({ tags, limit }); //to prevent infinite loop, if statement is used
-    }
-  }
+  async stateSetter() {}
   componentDidMount() {
-    this.stateSetter();
+    this.setTags(false);
   }
-  componentDidUpdate(prevProps) {
-    if (prevProps === this.props) return;
+  /*componentDidUpdate(prevProps, prevState) {
+    if (prevState.viewDead && prevState.viewDead === this.state.viewDead) {
+      return;
+    }
     this.stateSetter();
-  }
+  }*/
   handlePageChange = page => {
     this.setState({ currentPage: page });
   };
+  async setTags(viewDead) {
+    let { user } = this.props;
+    let tags = "";
+    if (user) {
+      let viewDeadBool = false;
+      if (viewDead === "Yes") viewDeadBool = true;
+      tags = await auth.getTags(viewDeadBool);
+
+      tags = tags.data.reverse();
+      //if (limit !== this.state.limit)
+      this.setState({ tags }); //to prevent infinite loop, if statement is used
+    }
+  }
+  setLimit() {
+    let { user } = this.props;
+    let limit = null;
+    if (user.tier === 0) limit = 5;
+    if (user.tier === 1) limit = 25;
+    return limit;
+  }
   render() {
     let { tags, limit, currentPage, pageSize } = this.state;
     if (!tags) tags = "";
-    if (tags[0] !== "" && tags.length < limit) tags.splice(0, 0, "");
+    if (tags != "" && tags.length < limit) tags.splice(0, 0, "");
     let sortedTags = paginate(tags, currentPage, pageSize);
-
-    function DeletePopup(data) {
-      const customText =
-        "This will permanently delete the tag titled " +
-        data.item.name +
-        ". Proceed?";
-      return (
-        <DeletePopupContainer
-          triggerText="Delete Tag"
-          customText={customText}
-          deleteRoute={deleteTag}
-          deleteData={data}
-        />
-      );
-    }
 
     async function deleteTag(data) {
       const props = data.props;
       const id = data.item._id;
       try {
         const result = await auth.deleteTag(id);
-        console.log(result);
         const { state } = props.location;
         if (result) window.location = state ? state.from.pathname : "/tags";
-      } catch (e) {
-        console.log(e);
-      }
+      } catch (e) {}
     }
+
     function customBehavior(item) {
       //css for customBehavior and emptyBehavior are in unit.css
       let redirect = this.state.redirect;
@@ -106,14 +94,18 @@ class Tags extends Component {
               className="unitLink mt-2 mb-2"
               onClick={() => {
                 const info = queryString.stringify({ id: item._id });
-                console.log(info);
                 redirect = "/printSheet/?" + info;
                 this.setState({ redirect });
               }}
             >
               Add to Printable Sheet
             </div>
-            <DeletePopup props={this.props} item={item} />
+            <CallbackPopupContainer
+              triggerText="Delete Tag"
+              customText={`This will permanently delete the tag titled: ${item.name}. Proceed?`}
+              callbackRoute={deleteTag}
+              callbackData={{ props: this.props, item: item }}
+            />
           </div>
           {redirect && (
             <Redirect
@@ -141,7 +133,6 @@ class Tags extends Component {
                 const result = await auth.newTag(
                   document.getElementById("input").value
                 );
-                await auth.genCache();
                 const { state } = this.props.location;
                 if (result) {
                   window.location = state ? state.from.pathname : "/tags";
@@ -167,6 +158,7 @@ class Tags extends Component {
             currentPage={currentPage}
             onPageChange={this.handlePageChange}
           />
+
           <Grid
             {...this.props}
             items={sortedTags}
