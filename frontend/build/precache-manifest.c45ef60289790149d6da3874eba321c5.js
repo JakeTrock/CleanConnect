06ffc5918d75f7ecbb20cd0e3a5d314cb0463bbd
Yoshterra1 <44651405+Yoshterra1@users.noreply.github.com>
2020-03-24 20:04:36 -0400

    Added braintree to package.json

---
+++ frontend/build/precache-manifest.c45ef60289790149d6da3874eba321c5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7698b3bb6f391b3ec1370d968bca96dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de7432293d1c9180b4df",
+    "url": "/static/css/2.54647d8a.chunk.css"
+  },
+  {
+    "revision": "cb1330d0fc39331f8136",
+    "url": "/static/css/main.7aaa1574.chunk.css"
+  },
+  {
+    "revision": "de7432293d1c9180b4df",
+    "url": "/static/js/2.8b671bd3.chunk.js"
+  },
+  {
+    "revision": "08fefedaab24d8dd1371108325f1f2ab",
+    "url": "/static/js/2.8b671bd3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cb1330d0fc39331f8136",
+    "url": "/static/js/main.429048c5.chunk.js"
+  },
+  {
+    "revision": "05e2d2637e83c42dc136",
+    "url": "/static/js/runtime-main.4ee28378.js"
+  },
+  {
+    "revision": "4174065a2c851d5a0a19d2856304dea9",
+    "url": "/static/media/FuturaMedium.4174065a.ttf"
+  }
+]);
